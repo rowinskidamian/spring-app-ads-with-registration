a6153439b04643a63a0757f0ec9ea6d97ed48a40
Damian Rowiński <damian.rowinski@gmail.com>
2020-09-09 14:08:05 +0200

    spring boot initialization

---
+++ src/main/java/pl/damianrowinski/springappadswithregistration/SpringAppAdsWithRegistrationApplication.java
@@ -0,0 +1,13 @@
+package pl.damianrowinski.springappadswithregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppAdsWithRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAppAdsWithRegistrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/damianrowinski/springappadswithregistration/SpringAppAdsWithRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.damianrowinski.springappadswithregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppAdsWithRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
